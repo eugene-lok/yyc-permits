74c1473335438f5b80592f8decb59ca8f45b60f1
Eugene Lok <eugeneywlok@gmail.com>
2021-03-05 14:57:33 -0700

    Change height, navbar styling

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
-// initialize our map
+// Initialize map
 var map = L.map('map', {
-    center:[-1.09713135, 37.014170107681], //center map to jkuat
-    zoom:10 //set the zoom level
+    // Center map at the City of Calgary
+    center:[51, -114], 
+    zoom: 10
 });
 
-//add openstreet baselayer to the map 
+//OpenStreetMap display
 var OpenStreetMap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+leaflet.control.scale().addTo(this.leaf);
 
 // define a blank geoJSON Layer
 var buildings = L.geoJSON(null);
 
 //get the geojson data with ajax, and add it to the blank layer we created
+/*
 $.getJSON('../data/bui.geojson',function(data){
 	buildings.addData(data);
 	map.fitBounds(buildings.getBounds());
@@ -22,4 +25,7 @@ $.getJSON('../data/bui.geojson',function(data){
 
 // finally add the layer to the map
 buildings.addTo(map);
+*/
+
+// Fetch Building Permit API
 
